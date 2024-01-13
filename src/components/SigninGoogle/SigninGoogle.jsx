db7a4eb7051051911e1d17bb7ed3d4fbc0eaa2f9
Swarup Chanda <swarupchanda1963@gmail.com>
2024-01-13 13:20:21 +0530

    Updated the changes

--- src/components/SigninGoogle/SigninGoogle.jsx
+++ src/components/SigninGoogle/SigninGoogle.jsx
@@ -2,56 +2,46 @@ import React from 'react';
 
 const SigninGoogle = () => {
   return (
-    <div>
-      <button>Sign In With Google</button>
-      <br />
-      <h4>Poster Image</h4>
-      <input type="file" name="image" id="" />
-      <br />
-      <h4>Description</h4>
-      <input type="text" />
-      <br />
-      <h4>Attendance Incentive</h4>
-      <input required type="text" />
-      <br />
-      <h4>Registration Incentive</h4>
-      <input required type="text" />
-      <br />
-      <h4>Latitude</h4>
-      <input type="text" />
-      <br />
-      <h4>Longitude</h4>
-      <input type="text" />
-      <br />
-      <h4>Max-team size</h4>
-      <input type="number" name="" id="" />
-      <br />
-      <h4>Min-team size</h4>
-      <input type="number" name="" id="" />
-      <br />
-      <h4>Module ID</h4>
-      <input type="text" />
-      <br />
-      <h4>Name</h4>
-      <input type="text" />
-      <br />
-      <h4>Prize Description</h4>
-      <input type="text" />
-      <br />
-      <h4>Registration End Time</h4>
-      <input type="datetime-local" name="" id="" />
-      <br />
-      <h4>Registration Start Time</h4>
-      <input type="datetime-local" name="" id="" />
-      <br />
-      <h4>Stages Description</h4>
-      <input type="text" name="" id="" />
-      <br />
-      <h4>Venue</h4>
-      <input type="text" />
-      <br />
-      <button>Submit</button>   
-    </div>
+    <form>
+      <div>
+        <button>Sign In With Google</button>
+      </div>
+      <div>
+        <label htmlFor="image">Poster Image</label>
+        <input type="file" name="image" id="image" />
+      </div>
+      <div>
+        <label htmlFor="description">Description</label>
+        <input type="text" id="description" />
+      </div>
+      <div>
+        <label htmlFor="attendanceIncentive">Attendance Incentive</label>
+        <input required type="text" id="attendanceIncentive" />
+      </div>
+      <div>
+        <label htmlFor="registrationIncentive">Registration Incentive</label>
+        <input required type="text" id="registrationIncentive" />
+      </div>
+      <div>
+        <label htmlFor="latitude">Latitude</label>
+        <input type="text" id="latitude" />
+      </div>
+      <div>
+        <label htmlFor="longitude">Longitude</label>
+        <input type="text" id="longitude" />
+      </div>
+      <div>
+        <label htmlFor="maxTeamSize">Max-team size</label>
+        <input type="number" id="maxTeamSize" />
+      </div>
+      <div>
+        <label htmlFor="minTeamSize">Min-team size</label>
+        <input type="number" id="minTeamSize" />
+      </div>
+      <div>
+        <button type="submit">Submit</button>
+      </div>
+    </form>
   );
 };
 
